85227f39e46ff70335344843b5e73d5b1bb0a5ec
Clarissa Shieh <clarissamshieh@gmail.com>
2025-04-05 17:33:31 -0500

    modified:   frontend/public/pictures/logo.png 	modified:   frontend/src/components/NavBar.jsx

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -8,7 +8,7 @@ function NavBar() {
           <div className="flex items-center">
             <div className="flex-shrink-0">
                 <img
-                  className=""
+                  className="w-40"
                   src="/pictures/logo.png"
                   alt="Logo"
                 />
