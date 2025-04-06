e5f2fb6fb5c780c3a7e5deb9875422fde871a4f2
Clarissa Shieh <clarissamshieh@gmail.com>
2025-04-06 03:34:58 -0500

    modified:   frontend/src/App.jsx 	modified:   frontend/src/components/AddPopUp.jsx 	modified:   frontend/src/components/AddTaskCard.jsx 	modified:   frontend/src/index.css 	modified:   frontend/src/pages/Habits.jsx 	modified:   frontend/src/pages/Login.jsx 	modified:   frontend/src/pages/Tasks.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,18 +3,21 @@ import {React, useState} from 'react'
 import Login from './pages/Login'
 import Tasks from './pages/Tasks'
 import Habits from './pages/Habits'
+import AddPopUp from './components/AddPopUp';
 
 import {PageContext} from './helpers/Contexts'
 
 function App() {
   const [pageState, setPageState] = useState("login");
+  const [popUpState, setPopUpState] = useState(false);
 
   return (
     <div className="font-jersey">
-      <PageContext.Provider value={{pageState, setPageState}}>
+      <PageContext.Provider value={{pageState, setPageState, popUpState, setPopUpState}}>
         {pageState === "login" && <Login />}
         {pageState === "tasks" && <Tasks />}
         {pageState === "habits" && <Habits />}
+        {popUpState === true && <AddPopUp />} 
       </PageContext.Provider>
     </div>
   )
--- frontend/src/pages/Tasks.jsx
+++ frontend/src/pages/Tasks.jsx
@@ -1,31 +1,27 @@
-import {React, useContext} from "react";
-import {PageContext} from '../helpers/Contexts'
+import {React} from "react";
 
 import TaskCard from '../components/taskCard'
 import NavBar from "../components/NavBar";
-import AddPopUp from "../components/AddPopUp";
-
+import AddTaskCard from "../components/AddTaskCard";
 
 function Tasks(){
-    const {pageState, setPageState} = useContext(PageContext);
     return(
         <div className="h-screen w-screen flex flex-col">
             <NavBar />
-            <AddPopUp />
-            <div className="text-5xl px-8 pt-8">Task HubHELLO</div>
+            <div className="text-5xl px-8 pt-8">Task Hub</div>
             <div className="text-3xl px-8">Complete your tasks to train your students!</div>
             <div className="p-8 w-full">
                 <div className="overflow-x-auto">
                     <div className="flex gap-8 pb-8">
-                        <TaskCard />
-                        <TaskCard />
-                        <TaskCard />
-                        <TaskCard />
-                        <TaskCard />
-                        <TaskCard />
-                        <TaskCard />
-                        <TaskCard />
                         <AddTaskCard />
+                        <TaskCard />
+                        <TaskCard />
+                        <TaskCard />
+                        <TaskCard />
+                        <TaskCard />
+                        <TaskCard />
+                        <TaskCard />
+                        <TaskCard />
                     </div>
                 </div>
             </div>
