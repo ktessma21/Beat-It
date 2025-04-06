6b1297e6c1ab8ec8d427d107c1165f944f9175c8
Kidus <ktessma21@gmail.com>
2025-04-05 22:44:22 -0500

    login
    Co-authored-by: AnaGRamirez <AnaGRamirez@users.noreply.github.com>

---
+++ frontend/src/pages/loginscript.js
@@ -0,0 +1,37 @@
+export async function loginUser(username, password, registrationtype) {
+    const url = "https://beatitbackend.onrender.com";
+
+    try {
+        if (registrationtype === "signup") {
+            const res = await fetch(`${url}/register`, {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ username, password }),
+            });
+            const data = await res.json();
+            if (!data.user_id) {
+                alert("Signup failed");
+                return null;
+            }
+        }
+
+        const res = await fetch(`${url}/login`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ username, password }),
+        });
+        const data = await res.json();
+
+        if (data.success && data.token) {
+            alert("Login successful!");
+            return data.token;
+        } else {
+            alert("Login failed.");
+            return null;
+        }
+    } catch (err) {
+        console.error("Error:", err);
+        return null;
+    }
+}
+export default loginUser; // Export the function for use in other files
